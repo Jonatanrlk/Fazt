780a395905a9d1c57acef4736060274f846d9c77
JonatanR <jonatanjr-2017@outlook.es>
2021-11-19 12:08:28 -0500

    movi la configuracion de las respuestas de mi base de datos a un archivo llmado indexroutes.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import express from 'express'
-
+import indexRoutes from './Routes/index.rautes'
 
 const app = express()
 
 app.listen(3000)
 console.log("server on port",3000)
 
-
-app.get('/',(req, res) =>{
-    res.send("Hello world")
-
-})
+app.use(indexRoutes)
